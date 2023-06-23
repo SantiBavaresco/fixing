01accc4110941fd70ee5082fa2314631078a8b1f
SantiBavaresco <santiago.bavaresco@codere.com>
2023-06-23 12:43:54 -0300

    2

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js too start working on your app!</Text>
+      <Text>Open up App.js to00 000 o start workingasda  aon your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
